195a1362c8a9de7b3bb1212e792bac71def0f67b
Faztudo-Machava <fasthymachava12@gmail.com>
2022-06-02 01:42:32 +0200

    Changing the mouse effect configurations

--- src/components/DownloadBtn.tsx
+++ src/components/DownloadBtn.tsx
@@ -2,7 +2,7 @@ import { Download } from "phosphor-react";
 
 export function DownloadCV(){
     return(
-        <a href="https://www.mediafire.com/file/h1p15d163nk89ib/Faztudo_Machava_CV_Ingl%25C3%25AAs.pdf/file" target={'_blank'}
+        <a href="https://drive.google.com/file/d/1LrfWnrZLdAvMElnv-ippmFfCKOQMVqOU/view?usp=sharing" target={'_blank'}
             className='border-2 border-principle-300 hover:bg-principle-300 btn-section whitespace-nowrap'
         >Baixar CV {<Download size={19} className='ml-2' weight="fill"/>}</a>
     );
